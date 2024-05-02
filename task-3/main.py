fd47e80469399c632cb51f8c90e017d22e126826
Arsene-Baitenov <arsenebaitenov@gmail.com>
2024-05-03 00:01:14 +0300

    Add experiment.

--- task-3/main.py
+++ task-3/main.py
@@ -1,5 +1,5 @@
-import os
 from pathlib import Path
+from pathos.pools import ProcessPool
 from compression import *
 
 
@@ -21,40 +21,31 @@ def process_file(ratio, bmppath, outbmpdir):
     end_time = time.time() * 1000
 
     new_image = np_compressor.from_svd(tmp)
-    new_image.save(os.path.join(outbmpdir, Path(bmppath).stem, f'numpy-{ratio}.BMP'))
+    new_image.save(os.path.join(outbmpdir, f'numpy-{ratio}.BMP'))
 
     pow_compressor = PowerSvdCompressor(end_time - start_time)
     tmp = pow_compressor.to_svd(bmppath, ratio)
 
     new_image = pow_compressor.from_svd(tmp)
-    new_image.save(os.path.join(outbmpdir, Path(bmppath).stem, f'power-{ratio}.BMP'))
+    new_image.save(os.path.join(outbmpdir, f'power-{ratio}.BMP'))
 
     bpow_compressor = BlockPowerSvdCompressor(end_time - start_time)
     tmp = bpow_compressor.to_svd(bmppath, ratio)
 
     new_image = bpow_compressor.from_svd(tmp)
-    new_image.save(os.path.join(outbmpdir, Path(bmppath).stem, f'bpower-{ratio}.BMP'))
+    new_image.save(os.path.join(outbmpdir, f'bpower-{ratio}.BMP'))
 
-
-def process_dir(ratio, bmpdir, outbmpdir):
-    files = [f for f in Path(bmpdir).iterdir() if f.is_file()]
-    for f in files:
-        process_file(ratio, f, outbmpdir)
-
-
-bmpdirnames = [
-    # 'bw',
-    # 'contrast',
-    # 'drops',
-    # 'geometry',
-    # 'gradient',
-    # 'lines'
-]
 
 currdir = os.getcwd()
 
-for bmpdirname in bmpdirnames:
-    process_dir(5,
-                os.path.join(currdir, 'source_bmp', bmpdirname),
-                os.path.join(currdir, 'out_bmp', bmpdirname))
+dirs = [d for d in Path(os.path.join(currdir, 'source_bmp')).iterdir() if d.is_dir()]
+src_bmp = []
+out_bmp_dirs = []
+for dir in dirs:
+    for f in Path(dir).iterdir():
+        if f.is_file() and f.suffix == '.bmp':
+            src_bmp.append(f.__str__())
+            out_bmp_dirs.append(os.path.join(currdir, 'out_bmp', Path(dir).name, Path(f).stem))
 
+for (src_bmp, out_bmp_dir) in zip(src_bmp, out_bmp_dirs):
+    process_file(5, src_bmp, out_bmp_dir)
